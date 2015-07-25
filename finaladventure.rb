8d9d971ccb4dfb08c029ed73a324ab8186c7dd85
sponee <kyle.sponheim@gmail.com>
2015-07-25 16:41:42 -0700

    A few stylistic changes and consolidations.

--- finaladventure.rb
+++ finaladventure.rb
@@ -11,33 +11,21 @@ puts "Hello, what's your name?"
 character_name = gets.chomp.to_s
 
 character = Characters::Character.new(character_name, 100) #creates character object with user input from character.rb
-mothra = Characters::Npcs::Mothra.new(250) #creates boss object named mothra from boss1.rb
+mothra = Characters::Npcs::Mothra.new(175) #creates boss object named mothra from boss1.rb
 die = Die.new
 encounter = ActionSelector.new
 
-puts "An enemy approaches with #{mothra.hp}HP, get ready!"
+puts "An enemy approaches with #{mothra.hp}HP get ready!"
+puts "You have #{character.hp}HP, #{character_name}."
 
 while mothra.hp >=1 && character.hp >=1
 	hp = rand(11..16) #fills the "hp" local variable in character.rb
 	encounter.print_options("Attack, Heal, or Flee #{character_name}?", ['attack', 'heal', 'flee'])
 	action = encounter.get_input
 
-	#   puts "Attack, Heal, or Flee #{character_name}?"
-	#   puts "Press 1 to attack"
-	#   puts "Press 2 to heal"
-	#   puts "Press 3 to flee"
-	#   action = gets.chomp.to_i
-
 	if action == 0 && mothra.hp > 1
 	encounter.print_options("Where do you strike?", ['head', 'torso', 'leg'])
 	amount = encounter.get_input
-
-	#	puts "Where do you strike?"
-	#	puts "Press 1 to strike it's head"
-	#	puts "Press 2 to strike it's torso"
-	#	puts "Press 3 to strike it's legs"
-
-	#amount = gets.chomp.to_i #fills the "amount" local variable in boss1.rb
 
 		if amount == 0
 			amount = 15
@@ -51,10 +39,8 @@ while mothra.hp >=1 && character.hp >=1
 		end
 
 		if mothra.hp > 1 && character.hp > 1
-			boss_hp = mothra.hp
-			puts "You attack ferociously. mothra has #{boss_hp}HP left."
+			puts "You attack ferociously. mothra has #{mothra.hp}HP left."
 			character.damage!(hp)
-			character_hp = character.hp
 		elsif mothra.hp < 1 && character.hp > 1
 			puts "You killed mothra, #{character_name}!"
 		end
@@ -62,14 +48,13 @@ while mothra.hp >=1 && character.hp >=1
 		if character.hp < 1
 			puts "You died, #{character_name}."
 		elsif character.hp > 1 && mothra.hp > 1
-			puts "mothra attacks ferociously. You have #{character_hp}HP left, #{character_name}."
+			puts "mothra attacks ferociously. You have #{character.hp}HP left, #{character_name}."
 		end
 
 	elsif action == 1 && mothra.hp > 1
 		character.damage!(hp)
 		character.heal!
-		character_hp = character.hp
-		puts "mothra attacks ferociously. You have #{character_hp}HP left, #{character_name}."
+		puts "mothra attacks ferociously. You have #{character.hp}HP left, #{character_name}."
 
 	elsif action == 2 && mothra.hp > 1
 		fleevalue = die.roll
@@ -80,12 +65,11 @@ while mothra.hp >=1 && character.hp >=1
 		elsif fleevalue != 1
 			hp = 25
 			character.damage!(hp)
-			character_hp = character.hp
 			puts "You failed to escape, #{character_name}, and mothra attacked ferociously."
 			if character.hp < 1
 				puts "You died, #{character_name}."
 			elsif character.hp > 1
-				puts "You have #{character_hp}HP left."
+				puts "You have #{character.hp}HP left."
 			end
 
 		end
