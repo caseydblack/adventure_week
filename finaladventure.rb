0e71d5ae1e2a293eae2f415f0a4f57d2750d9624
sponee <kyle.sponheim@gmail.com>
2015-07-28 20:57:23 -0700

    beginnings of rudimentary party system. Iterating over multiple character objects in a party array.

--- finaladventure.rb
+++ finaladventure.rb
@@ -9,22 +9,28 @@ puts "Hello, what's your name?"
 
 character_name = gets.chomp.to_s
 
-character = Characters::Character.new(character_name, 100) #creates character object with user input from character.rb
-mothra = Characters::Npcs::Mothra.new(125) #creates boss object named mothra from boss1.rb
+character = Characters::Character.new("Player One", 100) #creates character object from character.rb
+player_two = Characters::Character.new("Player Two", 100)
+mothra = Characters::Npcs::Mothra.new(250) #creates boss object named mothra from boss.rb
 die = Die.new
 encounter = ActionSelector.new
 
-puts "An enemy approaches with #{mothra.hp}HP get ready!"
-puts "You have #{character.hp}HP, #{character_name}."
+party = [character, player_two]
 
-while mothra.hp >=1 && character.hp >=1
-	hp = rand(13..18) #fills the "hp" local variable in character.rb
-	encounter.print_options("Attack, Heal, or Flee #{character_name}?", ['attack', 'heal', 'flee'])
+puts "An enemy approaches with #{mothra.hp}HP get ready!"
+
+party.each do |member|
+	puts "#{member.name} has #{member.hp}HP, #{character_name}."
+end
+
+while mothra.hp >=1
+	encounter.print_options("Attack, Heal, or Flee?", ['attack', 'heal', 'flee'])
 	action = encounter.get_input
 
 	if action == 0
-	encounter.print_options("Where do you strike?", ['head', 'torso', 'leg'])
-	amount = encounter.get_input
+		party.each do |member|
+			encounter.print_options("Where does #{member.name} strike?", ['head', 'torso', 'leg'])
+			amount = encounter.get_input
 
 		if amount == 0
 			amount = rand(16..21)
@@ -36,24 +42,35 @@ while mothra.hp >=1 && character.hp >=1
 			amount = rand(6..11)
 			mothra.subtract_hp(amount)
 		end
+	end
 
 		if mothra.hp > 1 && character.hp > 1
 			puts "You attack ferociously. mothra has #{mothra.hp}HP left."
-			character.damage!(hp)
+			party.each do |member|
+				hp = rand(13..18)
+				member.damage!(hp)
+			end
 		elsif mothra.hp < 1 && character.hp > 1
 			puts "You killed mothra, #{character_name}!"
 		end
 
 		if character.hp < 1
 			puts "You died, #{character_name}."
+
 		elsif character.hp > 1 && mothra.hp > 1
-			puts "mothra attacks ferociously. You have #{character.hp}HP left, #{character_name}."
+			party.each do |member|
+				puts "Mothra attacks #{member.name} ferociously. #{member.name} has #{member.hp}HP left."
+			end
 		end
 
 	elsif action == 1
-		character.damage!(hp)
-		character.heal!
-		puts "mothra attacks ferociously. You have #{character.hp}HP left, #{character_name}."
+		
+		party.each do |member|
+			hp = rand(13..18)
+			member.damage!(hp)
+			member.heal!
+			puts "mothra attacks ferociously. #{member.name} has #{member.hp}HP left, #{character_name}."
+		end
 
 	elsif action == 2
 		flee_value = die.roll
@@ -62,13 +79,14 @@ while mothra.hp >=1 && character.hp >=1
 			mothra.subtract_hp(1000)
 			puts "You ran away, #{character_name}. Very bold of you."
 		elsif flee_value != 1
-			hp = rand(21..26)
-			character.damage!(hp)
-			puts "You failed to escape, #{character_name}, and mothra attacked ferociously."
+			party.each do |member|
+				hp = rand(21..26)
+				member.damage!(hp)
+				puts "you failed to escape, #{character_name}, and Mothra fucked up #{member.name}"
+				puts "#{member.name} has #{member.hp}HP left."
+			end
 			if character.hp < 1
 				puts "You died, #{character_name}."
-			elsif character.hp > 1
-				puts "You have #{character.hp}HP left."
 			end
 
 		end
